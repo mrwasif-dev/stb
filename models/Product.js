8aa58e3638383119c67f933bbd96a2b76ce19cbf
mrwasif-dev <happyprincetemp@gmail.com>
2026-03-08 16:58:56 +0500

    Create Product.js

---
+++ models/Product.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    price: { type: Number, required: true },
+    category: { type: String, required: true },
+    description: String,
+    stock: { type: Number, default: 0 },
+    image: String,
+    available: { type: Boolean, default: true },
+    createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Product', productSchema);
